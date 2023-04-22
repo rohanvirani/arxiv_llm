09cf1ef1dcb566d780d4f954f904dc29f8a18370
rohanvirani <rohanvirani@hotmail.co.uk>
2023-04-21 23:08:18 -0700

    hey

--- amplify_llm.py
+++ amplify_llm.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
 from urllib.request import urlopen
-from llama_index import Document, GPTSimpleVectorIndex
+from llama_index import Document, GPTSimpleVectorIndex, ServiceContext
+from llama_index.llm_predictor import StableLMPredictor
 from bs4 import BeautifulSoup
 import os
 
@@ -12,7 +13,7 @@ import os
 # index over papers using llama index
 # prompt and learn about papers and coauthors 
 # serve up on front end 
-os.environ['OPENAI_API_KEY'] ="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"
+
 
 def gen_results(name, query):
     name_split = name.split()
@@ -27,8 +28,11 @@ def gen_results(name, query):
 
     for i in article_info:
         abstract_list.append(i.summary.text)
+    
+    stablelm_predictor = StableLMPredictor()
+    service_context = ServiceContext.from_defaults(chunk_size_limit=1024, llm_predictor=stablelm_predictor)
     documents = [Document(t) for t in abstract_list]
-    index = GPTSimpleVectorIndex.from_documents(documents)
+    index = GPTSimpleVectorIndex.from_documents(documents,service_context)
     response = index.query("What did the author do growing up?")
     return response
 
