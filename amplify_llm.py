875482d596fb81cf5b878cc83e01edb32bb88984
rohanvirani <rohanvirani@hotmail.co.uk>
2023-04-21 22:33:02 -0700

    changes

--- amplify_llm.py
+++ amplify_llm.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np 
 import urllib
 import feedparser
 from llama_index import Document, GPTSimpleVectorIndex
