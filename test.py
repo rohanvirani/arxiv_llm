db7c3f3c5f5f51ffb4083e082243dd291875e325
rohanvirani <rohanvirani@hotmail.co.uk>
2023-04-21 23:40:41 -0700

    hugging face

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 from llama_index import Document, GPTSimpleVectorIndex, ServiceContext
 from llama_index.llm_predictor import StableLMPredictor
 from typing_extensions import Protocol
+from langchain import HuggingFaceHub
 
 url = 'http://export.arxiv.org/api/query?search_query=au:"Percy%20Liang"&start=0&max_results=30&sortBy=submittedDate&sortOrder=descending'
 with urlopen(url) as response:
@@ -15,8 +16,10 @@ abstract_list = []
 
 for i in article_info:
     abstract_list.append(i.summary.text)
-stablelm_predictor = StableLMPredictor()
-service_context = ServiceContext.from_defaults(chunk_size_limit=1024, llm_predictor=stablelm_predictor)
+repo_id = "google/flan-t5-xl" # See https://huggingface.co/models?pipeline_tag=text-generation&sort=downloads for some other options
+
+llm = HuggingFaceHub(repo_id=repo_id, model_kwargs={"temperature":0, "max_length":64})
+service_context = ServiceContext.from_defaults(chunk_size_limit=1024, llm_predictor=llm)
 documents = [Document(t) for t in abstract_list]
 index = GPTSimpleVectorIndex.from_documents(documents,service_context)
 response = index.query("Explain these abstracts to a venture capitalist?")
