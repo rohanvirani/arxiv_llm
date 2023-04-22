5a8ca19e7ed909912dc880f4d69831c313d9b43e
rohanvirani <rohanvirani@hotmail.co.uk>
2023-04-21 23:15:50 -0700

    torch

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-from llama_index import Document, GPTSimpleVectorIndex
+from llama_index import Document, GPTSimpleVectorIndex, ServiceContext
+from llama_index.llm_predictor import StableLMPredictor
+from typing_extensions import Protocol
 
 url = 'http://export.arxiv.org/api/query?search_query=au:"Percy%20Liang"&start=0&max_results=30&sortBy=submittedDate&sortOrder=descending'
 with urlopen(url) as response:
@@ -13,8 +15,10 @@ abstract_list = []
 
 for i in article_info:
     abstract_list.append(i.summary.text)
+stablelm_predictor = StableLMPredictor()
+service_context = ServiceContext.from_defaults(chunk_size_limit=1024, llm_predictor=stablelm_predictor)
 documents = [Document(t) for t in abstract_list]
-index = GPTSimpleVectorIndex.from_documents(documents)
-response = index.query("What did the author do growing up?")
+index = GPTSimpleVectorIndex.from_documents(documents,service_context)
+response = index.query("Explain these abstracts to a venture capitalist?")
 print(response)
 
